231bb9eae510c31a0b2b44f1f1a4370099f8b1be
Tony Bradley <antbradley91@gmail.com>
2017-02-06 22:29:25 -0500

    stick it all into one file

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,50 @@
 module Main exposing (..)
 
-import Html exposing (Html)
-import Update exposing (Msg, Model, model, update, view)
+import Taco exposing (Taco, taco, TacoMsg(..))
+import Html exposing (..)
+import Pages.Home
+
+
+type Msg
+    = HomeMsg Pages.Home.HomeMsg
+
+
+type alias Model =
+    { taco : Taco
+    , home : Pages.Home.Model
+    }
+
+
+model : Model
+model =
+    { home = Pages.Home.model
+    , taco = taco
+    }
+
+
+updateModel : Msg -> Model -> ( Model, Cmd Msg, TacoMsg )
+updateModel msg model =
+    case msg of
+        HomeMsg msg ->
+            let
+                ( home, cmd, tacoMsg ) =
+                    Pages.Home.update model.home msg
+            in
+                ( { model | home = home }, Cmd.none, tacoMsg )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    let
+        ( newModel, cmds, tacoMsg ) =
+            updateModel msg model
+    in
+        ( { newModel | taco = Taco.update tacoMsg newModel.taco }, cmds )
+
+
+view : Model -> Html Msg
+view model =
+    Html.map HomeMsg (Pages.Home.view model.home)
 
 
 init : ( Model, Cmd Msg )
