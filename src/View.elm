86815eb66811aa581bae5d23fbe23ad49a1de2e8
Tony Bradley <antbradley91@gmail.com>
2017-02-06 21:12:39 -0500

    wtf

--- src/View.elm
+++ src/View.elm
@@ -1,10 +1,10 @@
 module View exposing (..)
 
 import Html exposing (Html, div, text)
-import Update exposing (Model, Msg, Msg(..))
+import Update exposing (Model, Msg(..))
 import Pages.Home
 
 
 view : Model -> Html Msg
 view model =
-    Html.map Msg (Pages.Home.view model)
+    Html.map HomeMsg (Pages.Home.view model.home)
