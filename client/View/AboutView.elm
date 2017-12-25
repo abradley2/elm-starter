39d8d14938d4d38543e4e1e6412623a78dfb28b1
Tony Bradley <antbradley91@gmail.com>
2017-12-25 11:37:11 -0500

    its running!

--- client/View/AboutView.elm
+++ client/View/AboutView.elm
@@ -4,10 +4,10 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Model exposing (Model)
-import Message exposing (Message)
+import Message.AboutMessage exposing (AboutMessage)
 
 
-aboutView : Model -> Html Message
+aboutView : Model -> Html AboutMessage
 aboutView model =
     div [ class "center measure" ]
         [ h3 [] [ text "About Page" ]
