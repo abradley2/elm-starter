6612985457d6cc8efb91517c7819deea95be61e9
Tony Bradley <antbradley91@gmail.com>
2017-12-26 12:03:25 -0500

    sidenav toggle

--- client/Layout.elm
+++ client/Layout.elm
@@ -1,27 +1,65 @@
 module Layout exposing (layout)
 
-import Css exposing (transform, translateX)
+import Css exposing (..)
+import Css.Colors
 import Html
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (..)
 import Html.Styled.Events exposing (..)
+import Message exposing (Message, Message(..))
+import Message.LayoutMessage exposing (LayoutMessage, LayoutMessage(..))
 import Model exposing (Model)
 
 
-layout : Model -> Html a -> Html a
+toggleSidenavButton : Html LayoutMessage
+toggleSidenavButton =
+    div
+        [ css
+            [ position absolute
+            , top (px 0)
+            , left (px 0)
+            , Css.width (px 56)
+            , Css.height (px 56)
+            , backgroundColor Css.Colors.red
+            , cursor pointer
+            ]
+        , onClick ToggleSidenav
+        ]
+        [ text "test"
+        ]
+
+
+sideNavtransform isOpen =
+    if isOpen then
+        "translateX(0%)"
+    else
+        "translateX(-105%)"
+
+
+layout : Model -> Html Message -> Html Message
 layout model view =
-    div [ class "center measure" ]
-        [ ul
-            [ id "slide-out"
-            , class "sidenav"
-            , style [ ( "transform", "translateX(0%)" ) ]
-            ]
-            [ li []
-                [ a [ href "#units" ] [ text "Units" ]
+    div
+        [ css
+            [ paddingTop (px 60) ]
+        ]
+        [ Html.Styled.map Layout toggleSidenavButton
+        , Html.Styled.map Layout
+            (ul
+                [ id "slide-out"
+                , class "sidenav"
+                , onClick ToggleSidenav
+                , style
+                    [ ( "transform", sideNavtransform model.layoutModel.sidenavOpen )
+                    , ( "transition", ".25s" )
+                    ]
                 ]
-            , li []
-                [ a [ href "#wargear" ] [ text "Wargear" ]
+                [ li []
+                    [ a [ href "#units" ] [ text "Units" ]
+                    ]
+                , li []
+                    [ a [ href "#wargear" ] [ text "Wargear" ]
+                    ]
                 ]
-            ]
+            )
         , view
         ]
